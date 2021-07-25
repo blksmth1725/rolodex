8cd7530ff890fb2a3d5894289246a2e477dcfc77
Christian Sheen <csheen1725@gmail.com>
2021-07-24 22:17:26 -0400

    Card background and transform effects...

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./card.styles.css";
 
 export const Card = (props) => {
   return (
-    <div>
-      <h1>{props.user.name}</h1>
+    <div className="card-container">
+      <img
+        alt="user"
+        src={`https://robohash.org/${props.user.id}?set=set2&size=180x180`}
+      />
+      <h2>{props.user.name}</h2>
+      <p>{props.user.email}</p>
     </div>
   );
 };
